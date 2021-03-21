af4d26d7f8a24c02d9c7493731e6c1d64fe6b936
Al-dasouqi <aldasouqif@gmail.com>
2021-03-21 14:12:38 +0200

    Lab 5 completed!

--- JS/javas.js
+++ JS/javas.js
@@ -1,5 +1,36 @@
 
- 
+ /* var userName="Al-dasouqi";
+ alert(userName);
+
+  var userName1= prompt("whats your name?");
+ console.log(userName1);
+
+ document.write('<h3>'+userName1+'</h3>');
+
+
+var favouriteAuthor= prompt("who's  your favourite Author?");
+
+
+
+ if(favouriteAuthor=='Dan Brown'){
+    document.write('<img src="https://mediad.publicbroadcasting.net/p/nhpr/files/styles/x_large/public/201711/dan_brown__.jpg">');
+}else if(favouriteAuthor=='Ali Al-wardi'){
+    document.write('<img src="https://th.bing.com/th/id/OIP.wEVu73oZ6pdrciVsfgHm9QAAAA?pid=ImgDet&rs=1">');
+}else{
+    alert('choose betwen Dan Brown or Ali Al-wardi next time');
+} 
+
+var age=prompt("whats your age?");
+
+console.log(age);
+if(age>=16){
+    alert('welcome to our club!'); 
+}else{
+    alert('Will wait you to join our club!');
+}
+confirm("Age confirmed!") 
+
+*/
 var age = prompt("what is your age?");
 
 console.log(age);
@@ -25,9 +56,9 @@ var pic = '';
 var result = '';
 
 if (favouriteWriter == 'Dan brown') {
-    painting = '<img src="https://mediad.publicbroadcasting.net/p/nhpr/files/styles/x_large/public/201711/dan_brown__.jpg"/> </br>';
+    pic = '<img src="https://mediad.publicbroadcasting.net/p/nhpr/files/styles/x_large/public/201711/dan_brown__.jpg"/> </br>';
 } else if (favouriteArtist == 'Ali Al-wardi') {
-    painting = '<img src="https://th.bing.com/th/id/OIP.wEVu73oZ6pdrciVsfgHm9QAAAA?pid=ImgDet&rs=1"/> </br>';
+    pic = '<img src="https://th.bing.com/th/id/OIP.wEVu73oZ6pdrciVsfgHm9QAAAA?pid=ImgDet&rs=1"/> </br>';
 }
 
 console.log(pic);
