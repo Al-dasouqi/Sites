205163ca5ab3e75a1f3fced3170b0869a7ff3465
Al-dasouqi <aldasouqif@gmail.com>
2021-03-29 16:36:08 +0300

    lab 2 done!

--- Class2/JS/app.js
+++ Class2/JS/app.js
@@ -28,9 +28,11 @@ let favouriteColor=prompt('My favourite color is blue?');
     console.log(favouriteColor);
 
  if (favouriteColor==='yes' || 'y') {
-   console.log('Sure! It\'s the color of the sky!');
+   //console.log('Sure! It\'s the color of the sky!');
+   alert('Sure! It\'s the color of the sky!');
  }else {
-   console.log('NO!!' );
+   //console.log('NO!!' );
+   alert('NO!!' );
  }
  //alert('If you answered YES that\'s correct!');
 
@@ -40,9 +42,11 @@ let favouriteColor=prompt('My favourite color is blue?');
     console.log(favouriteApp);
 
  if (favouriteApp=== 'yes' || 'y') {
-   console.log('Absouletley No!');
+   //console.log('Absouletley No!');
+   alert('Absouletley No!');
  }else {
-   console.log('I like INSTAGRAM!' );
+   //console.log('I like INSTAGRAM!' );
+   alert('I like INSTAGRAM!' );
  }
  //alert('If you answered YES that\'s correct!');
 
@@ -52,9 +56,11 @@ let favouriteColor=prompt('My favourite color is blue?');
     console.log(favouriteSeries);
 
  if (favouriteSeries==='yes' || 'y') {
-   console.log('YEEEEEES ! It\'s the best!');
+  // console.log('YEEEEEES ! It\'s the best!');
+   alert('YEEEEEES ! It\'s the best!');
  }else {
-   console.log('NO!!' );
+  // console.log('NO!!' );
+  alert('NO!!' );
  }
  //alert('If you answered YES that\'s correct!');
 
@@ -64,21 +70,26 @@ let favouriteColor=prompt('My favourite color is blue?');
     console.log(favouriteHabit);
 
  if (favouriteHabit==='yes' || 'y') {
-   console.log('Ofcourse NO!');
+   //console.log('Ofcourse NO!');
+   alert('Ofcourse NO!');
  }else {
-   console.log('NO ! I like reading!' );
+   //console.log('NO ! I like reading!' );
+   alert('NO ! I like reading!' );
+
  }
  //alert('If you answered YES that\'s correct!');
 
- let favouriteWriter=prompt('My favourite writer is DAN Brown?');
+ let favouriteWriter=prompt('My favourite writer is Dan Brown?');
     favouriteWriter=favouriteWriter.toLowerCase();
     console.log(favouriteWriter.toLowerCase());
     console.log(favouriteWriter);
 
  if (favouriteWriter==='yes' || 'y') {
-   console.log('Yes! he is the best');
+   //console.log('Yes! he is the best');
+   alert('Yes! he is the best');
  }else {
-   console.log('NO!! It\'s Dan brown ' );
+   //console.log('NO!! It\'s Dan brown ' );
+   alert('NO!! It\'s Dan brown ' );
  }
 
  alert(' Hi! ' + "My favourite color is Blue, I like spending time on Instagram,  My favourite series is FRIENDS!, I like reading and my favourite writer is Dan Brown!" );
---
+++ Class2/README.md
@@ -0,0 +1,7 @@
+#About-me Game
+1. My name is Fadia.
+2. My favourite APP is Instagram.
+3. My favourite color is Blue.
+4. My favourite series is Friends.
+5. I like reading.
+6. And my favourite writer is Dan brown.
