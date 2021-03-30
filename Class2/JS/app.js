7639ceff013a3d7f9df48d128eb083bedee25284
Al-dasouqi <aldasouqif@gmail.com>
2021-03-30 16:18:22 +0300

    lab 3 done!

--- Class2/JS/app.js
+++ Class2/JS/app.js
@@ -19,20 +19,22 @@
    console.log('hope you feel better soon');
  }
 
- alert("There is 5 questions you need to answer them in order to know how much you know me!");
+  alert("There is 5 questions you need to answer them in order to know how much you know me!");
 //alert('hellooo');
 
 let favouriteColor=prompt('My favourite color is blue?');
     favouriteColor=favouriteColor.toLowerCase();
     console.log(favouriteColor.toLowerCase());
     console.log(favouriteColor);
+    let score = 0;
 
- if (favouriteColor==='yes' || 'y') {
+ if (favouriteColor==='yes' || favouriteColor==='y') {
    //console.log('Sure! It\'s the color of the sky!');
    alert('Sure! It\'s the color of the sky!');
+   score++;
  }else {
    //console.log('NO!!' );
-   alert('NO!!' );
+   alert('NO!! , My favourite is Blue.' );
  }
  //alert('If you answered YES that\'s correct!');
 
@@ -41,12 +43,13 @@ let favouriteColor=prompt('My favourite color is blue?');
     console.log(favouriteApp.toLowerCase());
     console.log(favouriteApp);
 
- if (favouriteApp=== 'yes' || 'y') {
+ if (favouriteApp=== 'yes' || favouriteApp==='y') {
    //console.log('Absouletley No!');
-   alert('Absouletley No!');
+   alert('Absouletley it is not my favourite APP!');
  }else {
    //console.log('I like INSTAGRAM!' );
-   alert('I like INSTAGRAM!' );
+   alert('Yes ,I like INSTAGRAM!' );
+   score++;
  }
  //alert('If you answered YES that\'s correct!');
 
@@ -55,12 +58,13 @@ let favouriteColor=prompt('My favourite color is blue?');
     console.log(favouriteSeries.toLowerCase());
     console.log(favouriteSeries);
 
- if (favouriteSeries==='yes' || 'y') {
+ if (favouriteSeries==='yes' || favouriteSeries==='y') {
   // console.log('YEEEEEES ! It\'s the best!');
    alert('YEEEEEES ! It\'s the best!');
+   score++;
  }else {
   // console.log('NO!!' );
-  alert('NO!!' );
+  alert('NO!! it is my favourite' );
  }
  //alert('If you answered YES that\'s correct!');
 
@@ -69,12 +73,13 @@ let favouriteColor=prompt('My favourite color is blue?');
     console.log(favouriteHabit.toLowerCase());
     console.log(favouriteHabit);
 
- if (favouriteHabit==='yes' || 'y') {
+ if (favouriteHabit==='yes' || favouriteHabit==='y') {
    //console.log('Ofcourse NO!');
-   alert('Ofcourse NO!');
+   alert('Ofcourse NO! I don\'t like it');
  }else {
    //console.log('NO ! I like reading!' );
-   alert('NO ! I like reading!' );
+   alert('That\'s correct ! I like reading more!' );
+   score++;
 
  }
  //alert('If you answered YES that\'s correct!');
@@ -84,13 +89,69 @@ let favouriteColor=prompt('My favourite color is blue?');
     console.log(favouriteWriter.toLowerCase());
     console.log(favouriteWriter);
 
- if (favouriteWriter==='yes' || 'y') {
+ if (favouriteWriter==='yes' || favouriteWriter==='y') {
    //console.log('Yes! he is the best');
    alert('Yes! he is the best');
+   score++;
  }else {
    //console.log('NO!! It\'s Dan brown ' );
    alert('NO!! It\'s Dan brown ' );
  }
 
- alert(' Hi! ' + "My favourite color is Blue, I like spending time on Instagram,  My favourite series is FRIENDS!, I like reading and my favourite writer is Dan Brown!" );
 
+ let randomNum= parseInt(prompt("Please pick up a number form 1 to 10, Let\'s see if you will know the num I thinking about!"))
+  let answer = 2;
+  //let score = 0;
+    //console.log(randomNum,answer);
+   for ( let i=0 ; i<3 ; i++) {
+    // console.log(i+1);
+   if(randomNum === 2 )
+   
+   { 
+    //console.log(randomNum);
+       alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "
+               + answer + " GUESS ");
+               score++;
+              break;
+   } 
+   else if(randomNum > answer  ) /* if guessed number is greater
+                   than actual number*/
+   {    
+      // guess++;
+      // window.aler("OOPS SORRY!! TRY A SMALLER NUMBER");
+      randomNum=prompt("OOPS SORRY!! TRY A SMALLER NUMBER");
+   } 
+   else
+   {
+       //guess++;
+       //window.alert("OOPS SORRY!! TRY A GREATER NUMBER")
+       randomNum=prompt("OOPS SORRY!! TRY A GREATER NUMBER");
+   }
+  } 
+ alert(' Hi! ' + name + "The correct answer is 2, Play again!" );
+
+ let favWriter= prompt("If you know me , you know that I like two writers, guess one of them!")
+  let correct = ["Ali" , "Dan"];
+  favWriter=favWriter.toLowerCase();
+    //console.log(favWriter.toLowerCase());
+    //console.log(favWriter);
+    
+   for ( let i=0 ; i<5 ; i++) {
+    // console.log(i+1);
+   if(favWriter === correct[0] || favWriter === correct[1] )
+   {    
+       alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "
+               + correct + " GUESS ");
+               score++
+               break;
+   } 
+   else 
+   {    
+      // guess++;
+      // window.aler("OOPS SORRY!! TRY A SMALLER NUMBER");
+      favWriter=prompt("OOPS SORRY!! TRY AGAIN");
+   } 
+  
+  } 
+ alert(' Hi! ' + name + "The correct answer is Ali Al-wardi or Dan Brown, Play again!" );
+ alert(`thanks for playing ${name} your score is ${score} `);
